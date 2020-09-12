b51029b39fed6c726830dd5c8939e325210d09e0
kellyCristinaAires <k.woys@hotmail.com>
2020-09-12 15:38:49 -0300

    atividade cidades

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace atividadecidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Santos");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Itariri");
+            Console.WriteLine("Peruibe");
+            Console.WriteLine("Itariri");
+        }
+    }
+}
